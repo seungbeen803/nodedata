46dd7dcc5ea45f92a20f81cca735119a1afbf5da
seungbeen <seungbin803@gmail.com>
2023-03-14 20:04:44 +0900

    배열 예제

---
+++ node1/arrayEx1.js
@@ -0,0 +1,13 @@
+// 배열 선언 방법 두 가지
+// 대괄호(대표)
+const numbers = [1, 2, 3];
+const strings = ['hello', 'bye', 'welcome'];
+
+// new Array()
+const numbers2 = new Array(1, 2, 3);
+const strings2 = new Array('hello', 'bye', 'welcome');
+
+console.log('numbers:', numbers);
+console.log('numbers2:', numbers2);
+console.log('strings:', strings);
+console.log('strings2:', strings2);
