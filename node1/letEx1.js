6cc2df992ebb0ad2e4b73b9fabee7bb37196e3b8
seungbeen <seungbin803@gmail.com>
2023-03-14 19:58:31 +0900

    let, var 예제

---
+++ node1/letEx1.js
@@ -0,0 +1,15 @@
+// let, var의 차이점 알기
+let fruit = 'apple';
+console.log('fruit:', fruit);
+
+fruit = 'grape';
+console.log('fruit:', fruit);
+
+// let은 같은 이름의 변수를 선언할 수 없다.
+// let fruit = 'hello';
+
+// var는 같은 이름의 변수를 선언할 수 있다.
+var variable1 = 'hello';
+var variable1 = 'bye';
+
+console.log(variable1);
--- node1/test1.js
+++
@@ -1,7 +0,0 @@
-// console : Node의 출력
-// 디버깅, 로그를 남길때 이용
-// console.error(), console.warn()
-
-console.log('안녕하세요...');
-console.log('수지입니다. %d', 10);
-console.log('문자열입니다. %s', '안녕');
