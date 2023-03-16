ee542f6a9ddef56a695a4acc3c595d99ba98a1a3
seungbeen <seungbin803@gmail.com>
2023-03-16 19:29:51 +0900

    화살표 함수(arrow function)

--- node2/n11_define_function.js
+++ node2/n11_define_function.js
@@ -10,3 +10,17 @@ const printBye = () => {
   console.log('bye');
 }
 printBye();
+
+// 화살표 함수(arrow function)
+// let func = function(arg1, arg2, ...) {
+//   return 표현식;
+// }
+
+// let func = (arg1, arg2, ...) => 표현식
+// -> 인자 arg1, arg2를 받는 함수 func이 만들어짐.
+// func는 화살표 우측의 표현식을 평가하고 결과를 반환.
+
+let sum = (a, b) => {
+  return a + b;
+}
+console.log(sum(8, 3));
