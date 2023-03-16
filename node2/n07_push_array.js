e29c9e32c75d03476ebb658d0d5ff614abcf9631
seungbeen <seungbin803@gmail.com>
2023-03-16 18:50:50 +0900

    push()를 이용하여 배열에 값을 넣는다

---
+++ node2/n07_push_array.js
@@ -0,0 +1,11 @@
+// push()를 이용하여 배열에 값을 넣는다
+const arNumbers = [];
+arNumbers.push(1);
+arNumbers.push(2);
+arNumbers.push(3);
+
+const arTexts = [];
+arTexts.push('hello', 'welcome', 'bye');
+
+console.log(arNumbers);
+console.log(arTexts);
