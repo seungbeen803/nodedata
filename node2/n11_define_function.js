6645f854e3d6b4467b07c58ba787fa9c6ec36164
seungbeen <seungbin803@gmail.com>
2023-03-16 19:26:41 +0900

    const 함수명 = () => { };

---
+++ node2/n11_define_function.js
@@ -0,0 +1,12 @@
+// ES6에서는 function구문대신 => 를 사용해 함수 작성
+// function을 빼고 () 다음에 => 를 써준다.
+
+function printHello() {
+  console.log('hello');
+}
+printHello();
+
+const printBye = () => {
+  console.log('bye');
+}
+printBye();
