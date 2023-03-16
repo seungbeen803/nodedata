959af10270048ec6af2301a0c1b16bb7a2a64e33
seungbeen <seungbin803@gmail.com>
2023-03-16 18:55:51 +0900

    JSON(JavaScript Object Notation)

---
+++ node2/n08_json.js
@@ -0,0 +1,10 @@
+// JSON(JavaScript Object Notation)
+const user = {};
+user.name = 'kyeongrok';
+user.age = 31;
+
+console.log(user);
+
+// key에 해당하는 값에 원래는 ""를 붙여주어야 하지만 코딩 편의상 붙혀주지 않아도 됩니다.
+const user2 = { name: 'kyeongrok', age: 31};
+console.log(user2);
