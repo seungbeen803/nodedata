46628c45a7e887a937b95d34d127426c069c7c87
seungbeen <seungbin803@gmail.com>
2023-03-16 20:01:57 +0900

    module 모듈

---
+++ node2/main.js
@@ -0,0 +1,6 @@
+// main.js : 모듈을 불러오는 함수
+// 생성한 모듈을 다름 자바스크립트 파일에서 모듈을 추출할 때 require
+var module = require( './module.js ');
+
+console.log('abs(-273) = %d', module.abs(-273));
+console.log('circleArea(3) = %d', module.circleArea(3));
---
+++ node2/modules.js
@@ -0,0 +1,13 @@
+// modules.js : 생성할 모듈 파일, 모듈을 생성할 대 exports 사용
+// 절대값을 구하는 메서드
+exports.abs = function (number) {
+  if(0 < number) {
+    return number;
+  } else {
+    return -number;
+  }
+};
+// 원의 넓이를 구하는 메서드
+exports.circleArea = function (radius) {
+  return radius * radius * Math.PI;
+}
