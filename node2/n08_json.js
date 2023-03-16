423101efaa7b54b6197e5f1e0912ad2f65e420fa
seungbeen <seungbin803@gmail.com>
2023-03-16 19:04:21 +0900

    json 객체에서 값을 추출하려면 [ ] 또는, 연산자를 이용

--- node2/n08_json.js
+++ node2/n08_json.js
@@ -1,10 +1,22 @@
 // JSON(JavaScript Object Notation)
-const user = {};
-user.name = 'kyeongrok';
-user.age = 31;
+// const user = {};
+// user.name = 'kyeongrok';
+// user.age = 31;
 
-console.log(user);
+// console.log(user);
 
 // key에 해당하는 값에 원래는 ""를 붙여주어야 하지만 코딩 편의상 붙혀주지 않아도 됩니다.
 const user2 = { name: 'kyeongrok', age: 31};
 console.log(user2);
+
+const user = { name: "kim", age: 31 };
+console.log('user:', user);
+console.log('user.name:', user.name);
+console.log('user.age:', user.age);
+
+user.job = 'developer';
+user.nation = 'korea';
+console.log(user);
+
+const fieldName = 'age';
+console.log(user[fieldName]);
