b19ff4fb12275cd0cfa2ae3f6d1a9ea8df6ac040
seungbeen <seungbin803@gmail.com>
2023-03-21 19:36:23 +0900

    process 정보를 다루는 객체

---
+++ node3/ch02_test3.js
@@ -0,0 +1,14 @@
+// process 객체는 프로세스 정보를 다루는 객체
+// argv : 프로세스를 실행할 때 전달되는 파라미터(매개변수)정보
+// env : 환경변수 정보
+// exit() : 프로세스를 끝내는 메서드
+
+console.log('argv 속성의 파라미터 수 : ' + process.argv.length);
+console.dir(process.argv);
+
+process.argv.forEach(function(item, index) { // index, item은 변수
+  console.log(index + ' : ', item);
+});
+
+// console.dir(process.env);
+// console.log('os 환경변수의 값 : ' + process.env['os']);
