8df0aa1031b5173b234bafd843fa5f58003c39a9
seungbeen <seungbin803@gmail.com>
2023-03-28 20:03:14 +0900

    http2

--- node5/n25_http2.js
+++ node5/n25_http2.js
@@ -4,7 +4,7 @@ let http = require('http');
 // 웹서버 객체를 만듭니다.
 let server = http.createServer();
 
-// 웹 버를 시작하여 3000번 포트에서 대기하도록 합니다.
+// 웹서버를 시작하여 3000번 포트에서 대기하도록 합니다.
 let port = 4000;
 server.listen(port, function() {
   console.log('웹서버가 시작되었습니다. : %d', port);
