496d3878e115916cb8afd2ba91196894c04afb1e
seungbeen <seungbin803@gmail.com>
2023-04-13 19:34:05 +0900

    app.js 작성하기 서버 구동의 핵심이 되는 파일 app.set('port', 포트)로 서버가 실행될 포트 지정 app.get('주소', 라우터)로 GET 요청이 들어올 때

--- node9/express01.js
+++ node9/express01.js
@@ -1,3 +1,8 @@
+// app.js 작성하기
+// 서버 구동의 핵심이 되는 파일
+// app.set('port', 포트)로 서버가 실행될 포트 지정
+// app.get('주소', 라우터)로 GET 요청이 들어올 때
+
 const express = require('express');
 const app = express()
 const port = 3003
